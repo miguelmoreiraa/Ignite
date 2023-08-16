e226916c938c70fb8b04816be04ee20ed920f4fb
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2023-08-16 00:11:48 -0300

    Changes code

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/miguelmoreiraa.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Miguel Moreira</strong>
+              <time title="11 de Maio ás 08:13" dateTime="2022-05-01 08:13:30">
+                Cerca de 2h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
