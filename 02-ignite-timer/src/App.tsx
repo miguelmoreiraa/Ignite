3a488eb25f958cddf2416c6457999f674aacc75c
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2024-03-12 22:24:30 -0300

    Created home

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,18 +1,16 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/theme/default";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant={"primary"} />
-      <Button variant={"secondary"} />
-      <Button variant={"success"} />
-      <Button variant={"danger"} />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
